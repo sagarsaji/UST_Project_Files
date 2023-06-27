7f1397d2c4d42454f5e8428127f2d9e9ad5aa1f5
Sagar Saji(UST <245195@ust.com>
2023-06-27 15:01:06 +0530

    backend update

--- Backend/authentication-service/src/main/java/com/cravio/authenticationservice/AuthenticationServiceApplication.java
+++ Backend/authentication-service/src/main/java/com/cravio/authenticationservice/AuthenticationServiceApplication.java
@@ -2,9 +2,10 @@ package com.cravio.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
